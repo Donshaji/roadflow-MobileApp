97de0b122b437d04224dcd88ba8c6c5cf960a666
don <donpattamana@gmail.com>
2024-02-03 18:42:40 +0530

    Update main.dart

--- roadflow/lib/main.dart
+++ roadflow/lib/main.dart
@@ -35,8 +35,51 @@ class MyApp extends StatelessWidget {
           style:TextStyle(color: Colors.white)),
            // Set the title of the AppBar
         ),
-         body:Stack(),
-        ),
+
+
+         body:OSMFlutter( 
+        controller:mapController,
+        osmOption: OSMOption(
+              userTrackingOption: const UserTrackingOption(
+              enableTracking: true,
+              unFollowUser: false,
+            ),
+            zoomOption: const ZoomOption(
+                  initZoom: 8,
+                  minZoomLevel: 3,
+                  maxZoomLevel: 19,
+                  stepZoom: 1.0,
+            ),
+            userLocationMarker: UserLocationMaker(
+                personMarker: const MarkerIcon(
+                    icon: Icon(
+                        Icons.location_history_rounded,
+                        color: Colors.red,
+                        size: 48,
+                    ),
+                ),
+                directionArrowMarker: const MarkerIcon(
+                    icon: Icon(
+                        Icons.double_arrow,
+                        size: 48,
+                    ),
+                ),
+            ),
+            roadConfiguration: const RoadOption(
+                    roadColor: Colors.yellowAccent,
+            ),
+            markerOption: MarkerOption(
+                defaultMarker: const MarkerIcon(
+                    icon: Icon(
+                      Icons.person_pin_circle,
+                      color: Colors.blue,
+                      size: 56,
+                    ),
+                )
+            ),
+        )
+    ),
+    ),
       );
   }
 }
