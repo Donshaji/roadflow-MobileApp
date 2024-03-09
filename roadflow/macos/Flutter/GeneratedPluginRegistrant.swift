b00fa56440efae0e002560350808fa0db7a468e5
don <donpattamana@gmail.com>
2024-03-09 11:00:49 +0530

    firebase link

--- roadflow/macos/Flutter/GeneratedPluginRegistrant.swift
+++ roadflow/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import geolocator_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
